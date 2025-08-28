156b97f378482b2c14598632222d91fe1debb203
ORLANDO CLINIO PATRIARCHA <orlando.cpatriarcha@senacsp.edu.br>
2025-08-28 09:08:47 -0300

    initial load

---
+++ Blog/src/test/java/br/com/senac/sp/stads4ma/dsw/Blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.sp.stads4ma.dsw.Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
